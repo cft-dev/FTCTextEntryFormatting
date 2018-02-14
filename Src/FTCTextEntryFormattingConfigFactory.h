2ac109d90316e86ab4b4753e95d06bee892c8c37
Andrey Dirsha <mrhard4@gmail.com>
2018-02-14 11:49:13 +0300

    Fix import error

--- Src/FTCTextEntryFormattingConfigFactory.h
+++ Src/FTCTextEntryFormattingConfigFactory.h
@@ -18,6 +18,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#import "FTCTextEntryFormatting.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
 @class FTCTextEntryFormattingConfig;
